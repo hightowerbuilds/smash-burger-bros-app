308bc37629e7afaad431f9e2e3e399f9b68de060
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-09-21 09:51:40 -0700

    clearing for home page redo

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,5 @@
 
-
-import perfection from '../../images/browser/happyHomer.jpg'
-import carCrusher from '../../images/browser/carCrusher.png'
 import cheeseburger from '../../images/browser/cheeseburger.png'
-import happyCow from '../../images/browser/happyCow.jpeg'
-import foodTruck from '../../images/browser/foodTruck.png'
 import NavBar from '../../components/NavBar/NavBar'
 import './HomePage.css'
 
@@ -18,13 +13,13 @@ export default function HomePage() {
 
     <div className='homePageSectionOuterBox'>
       <div className='homePageSectionSubBox'>
-      <img src={cheeseburger} />
+    
       </div>
     </div>
   
 <div className='homePageSectionOuterBox'>
   <div className='homePageDescription'>
-    <h2 style={{ marginBottom: '1%'}}>Burgers so good you'll literally have a million!</h2>
+   
  
   </div>
  
@@ -35,8 +30,7 @@ export default function HomePage() {
 
 <div className='homePageSectionOuterBox'>
   <div className='homePageDescription'>
-    <h2 style={{ marginBottom: '1%'}}>THEY MAKE YOU SMARTER TOO??? test</h2>
-
+    
   </div>
     </div>
     </div>  
